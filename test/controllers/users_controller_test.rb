02179aac72f05246ec9db34004fc8aa7f6ad8939
mbrand12 <mbrand12.dump@gmail.com>
2015-10-28 12:04:29 +0100

    Modify `UsersControllerTest`

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,14 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  test "should get new" do
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
+  test "gets sign up page" do
     get :new
     assert_response :success
+    assert_select "title", "Sign up | #{@base_title}"
   end
 
 end
