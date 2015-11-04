1f972f9e0ce154e16d6e82bc9fd5feb5cd220a66
mbrand12 <mbrand12.dump@gmail.com>
2015-11-04 19:14:07 +0100

    Permanent Session and "Remember me" functionality
    Also some tweaks concerning Bootstrap 4
    
    Some test names might be different as well since I am trying out
    different naming styles.

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -38,4 +38,26 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
   end
+
+  test "user logging out in the second window as well" do
+    get login_path
+    post login_path, session: { email: @user.email, password: 'password' }
+    assert is_logged_in?
+
+    delete logout_path
+    assert_not is_logged_in?
+
+    delete logout_path
+    assert_redirected_to root_url
+  end
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: "1")
+    assert_not_nil cookies["remember_token"]
+  end
+
+  test "login without remembering" do
+    log_in_as(@user, remember_me: "0")
+    assert_nil cookies["remember_token"]
+  end
 end
