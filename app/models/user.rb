980eb3921c7f2049c67a378dd93c93af03e2d0ec
mbrand12 <mbrand12.dump@gmail.com>
2015-11-04 16:44:26 +0100

    Add Log in functionality and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,12 @@ class User < ActiveRecord::Base
                        length: { minimum: 6 }
 
   before_save { self.email = email.downcase }
+
+  # returns the hash digest of a given string
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
 
   test "flash message persistence over page's render" do
     get login_path
@@ -12,5 +15,17 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
     get root_path
     assert flash.empty?
+  end
+
+  test "header nav links changing on successful login" do
+    get login_path
+    post login_path, session: { email: @user.email, password: "password"}
+    assert_redirected_to @user
+
+    follow_redirect!
+    assert_template "users/show"
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
   end
 end
