f7dcf345bc1b3884e1d362c3f92d28667b3e1d81
mbrand12 <mbrand12.dump@gmail.com>
2015-11-07 19:41:11 +0100

    Account Activation resources

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
   validates :name, presence: true,
                    length: { maximum: 50 }
@@ -17,7 +17,8 @@ class User < ActiveRecord::Base
                        length: { minimum: 6 },
                        allow_nil: true
 
-  before_save { self.email = email.downcase }
+  before_save   :downcase_email
+  before_create :create_activation_digest
 
   # returns the hash digest of a given string
   def self.digest(string)
@@ -49,4 +50,15 @@ class User < ActiveRecord::Base
     update_attribute(:remember_digest, nil)
   end
 
+  private
+
+    # Converts email to downcase
+    def downcase_email
+      self.email = email.downcase
+    end
+
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_token = User.digest(activation_token)
+    end
 end
