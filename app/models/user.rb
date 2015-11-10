07953dd7357d2676d9786d549ac02cc44be598cd
mbrand12 <mbrand12.dump@gmail.com>
2015-11-10 14:03:03 +0100

    Reseting the password

--- app/models/user.rb
+++ app/models/user.rb
@@ -72,6 +72,12 @@ class User < ActiveRecord::Base
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
   end
+
+  # Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
   private
 
     # Converts email to downcase
