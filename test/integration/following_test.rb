c37f29adce77129b4b40f1862d3e5fbd4586a11f
mbrand12 <mbrand12.dump@gmail.com>
2015-11-16 11:13:29 +0100

    Working follow/unfollow button

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class FollowingTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
+    @other = users(:archer)
     log_in_as @user
   end
 
@@ -25,6 +26,34 @@ class FollowingTest < ActionDispatch::IntegrationTest
 
     @user.followers.each do |user|
       assert_select "a[href=?]", user_path(user)
+    end
+  end
+
+  test "follow a user the standard way" do
+    assert_difference '@user.following.count', 1 do
+      post relationships_path, followed_id: @other.id
+    end
+  end
+
+  test "follow a user with Ajax" do
+    assert_difference '@user.following.count', 1 do
+      xhr :post, relationships_path, followed_id: @other.id
+    end
+  end
+
+  test "unfollow a user the standard way" do
+    @user.follow(@other)
+    relationship = @user.active_relationships.find_by(followed_id: @other.id)
+    assert_difference '@user.following.count', -1 do
+      delete relationship_path(relationship)
+    end
+  end
+
+  test "unfollow a user with Ajax" do
+    @user.follow(@other)
+    relationship = @user.active_relationships.find_by(followed_id: @other.id)
+    assert_difference '@user.following.count', -1 do
+      xhr :delete, relationship_path(relationship)
     end
   end
 end
